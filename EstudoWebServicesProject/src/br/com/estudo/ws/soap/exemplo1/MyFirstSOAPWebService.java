e4bdbeb10306f109350562d7dcfd4937c8db12ba
anderson luiz pereira <andy2903.alp@gmail.com>
2016-03-16 23:35:36 -0300

    Comentários

--- EstudoWebServicesProject/src/br/com/estudo/ws/soap/exemplo1/MyFirstSOAPWebService.java
+++ EstudoWebServicesProject/src/br/com/estudo/ws/soap/exemplo1/MyFirstSOAPWebService.java
@@ -7,16 +7,21 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.validation.constraints.NotNull;
 
+//Declaração do WebService com anotação @WebService
 @WebService
 public class MyFirstSOAPWebService {
 
+	//Declaração do WebMethod com anotação @WebMethod, onde se, cria as classes
+	//com os parametros exemplo @WebParam(name="nome"), com o propriedade, com o nome embutido
+	//Permitindo valor nulo ou não com @NotNull
 	@WebMethod
 	public String ping(@WebParam(name="nome")@NotNull String nome){
 		
 		return "Pong !" +nome;
 		
 	}
-	
+	//Você consegue indicar o nome do método operationName="MostreMinhaIdade"
+	//caso, queira excluir o método, tem que inserir o exclude
 	@WebMethod(operationName="MostreMinhaIdade",exclude=true)
 	public Integer getAge(){
 		
